237ed2de3b16e9df4505835171805c1cf49d1c27
anton1732 <anton.alejandro@fusionacademy.me>
2025-06-12 10:43:46 -0500

    Create year.py

---
+++ year.py
@@ -0,0 +1,16 @@
+print("Person A will tell us their birth year first")
+
+print("Person B will tell us their birth year second")
+
+print("I, the computer program, will tell you who is older! Hold your applause, please")
+print("Person A what is your birth year")
+personA = int(input())
+print("what is person B birth year")
+personB = int(input())
+
+# REMEMBER, YOU WILL HAVE TO USE int(input....)
+# don't forget the int(...)
+
+# after checking to see which birth year makes the older person older, print:
+#"Person A is older! You're welcome"
+#"Person B is older! You're welcome"
